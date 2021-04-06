be4a8be00688de4d6762c3c29034c70b7c2caa54
Andrea Mancini <mancioman2@gmail.com>
2021-04-06 21:31:29 +0200

    try to add certbot

--- multiarch.sh
+++ multiarch.sh
@@ -2,6 +2,9 @@
 TAG="mancioman2/simplepage:latest"
 PLATFORMS="linux/amd64,linux/arm64,linux/arm/v6,linux/arm/v7"
 
+echo "--- BUILD THE CONTAINER BUILDER --"
 docker buildx create --name mbuilder
+echo "-- USE THE BUILDER --"
 docker buildx use mbuilder
+echo "-- BUILD ON PLATFORMS: $PLATFORMS AND PUSH TO: $TAG"
 docker buildx build --platform $PLATFORMS -t $TAG --push .
